2cadd70cbd420bb269f351254d2e9a38012d02a4
f <f>
2013-11-05 11:08:40 -0200

    updated license

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,9 @@
-/*	Responsive Aeon Grid v2.0
-	Designed & Built by Fernando Monteiro, http://www.newaeonweb.com.br	
-	Licensed under GPL license, http://www.gnu.org/licenses/gpl-3.0-standalone.html	
+/*
+    Responsive Aeon Grid System v1.0.0
+    Designed & Built by Fernando Monteiro, http://www.newaeonweb.com.br 
+    Licensed under MIT license, http://opensource.org/licenses/mit-license.php
+    
+    Base JavaScript File
 */
 
 //Basic script for Responsive navigation
