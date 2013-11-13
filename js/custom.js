1703a359ec117219521f651deda7ccabd482fa69
f <f>
2013-11-13 16:25:52 -0200

    adjust space indentation

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,18 @@
 /*
-    Responsive Aeon Grid System v1.0.0
-    Designed & Built by Fernando Monteiro, http://www.newaeonweb.com.br 
-    Licensed under MIT license, http://opensource.org/licenses/mit-license.php
-    
-    Base JavaScript File
+		Responsive Aeon Grid System v1.0.0
+		Designed & Built by Fernando Monteiro, http://www.newaeonweb.com.br 
+		Licensed under MIT license, http://opensource.org/licenses/mit-license.php
+		
+		Base JavaScript File
 */
 
 //Basic script for Responsive navigation
 
 $(document).ready(function () {
-  selectnav('nav', {
-		  label: '--- Table of content --- ',//first select field
-		  nested: true,
-		  indent: '-'
-	 });
+	selectnav('nav', {
+			label: '--- Table of content --- ',//first select field
+			nested: true,
+			indent: '-'
+	});
 });
 //Add your scripts here
